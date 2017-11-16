ed392e9a22f85d7de14867087464cb3267ea86ab
Noam Raz <noamraz8@gmail.com>
2017-11-16 20:03:53 +0200

    Extract location marker creation into separate function

--- mparse/src/loc_printing.cpp
+++ mparse/src/loc_printing.cpp
@@ -1,13 +1,23 @@
 #include "loc_printing.h"
 
+#include <algorithm>
 #include <iostream>
 #include <string>
+#include <type_traits>
+
+namespace {
+
+std::string make_marker(std::size_t size) {
+  return std::string(size, '~');
+}
+
+}  // namespace
 
 void print_loc(mparse::source_range where, std::string_view source) {
   std::cout << source << '\n';
 
   std::cout << std::string(where.from(), ' ')
-    << std::string(where.to() - where.from(), '~') << '\n';
+    << make_marker(where.to() - where.from()) << '\n';
 }
 
 void print_locs(const std::vector<mparse::source_range>& locs, std::string_view source) {
@@ -16,7 +26,7 @@ void print_locs(const std::vector<mparse::source_range>& locs, std::string_view 
   mparse::source_range prev_loc;
   for (const auto& loc : locs) {
     std::cout << std::string(loc.from() - prev_loc.to(), ' ')
-      << std::string(loc.to() - loc.from(), '~');
+      << make_marker(loc.to() - loc.from());
     prev_loc = loc;
   }
   std::cout << '\n';
