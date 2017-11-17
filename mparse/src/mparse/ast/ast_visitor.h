bf99f26d897497d386ff718cea386b665b7d89e0
Noam Raz <noamraz8@gmail.com>
2017-11-18 00:10:54 +0200

    Remove ast_node_cast
    It is better to simply use a visitor directly.

--- mparse/src/mparse/ast/ast_visitor.h
+++ mparse/src/mparse/ast/ast_visitor.h
@@ -20,33 +20,4 @@ struct ast_visitor {
   virtual void visit(literal_node&) {}
 };
 
-
-template<typename To, typename From>
-To* ast_node_cast(From* node) {
-  static_assert(std::is_base_of_v<ast_node, To>, "Node casts are only supported to AST nodes");
-  static_assert(std::is_base_of_v<ast_node, From>, "Node casts are only supported from AST nodes");
-
-  if constexpr (std::is_base_of_v<To, From>) {  // upcast
-    return static_cast<To*>(node);
-  } else {
-    static_assert(std::is_base_of_v<From, To>, "ast_node_cast does not support sibling casts");
-
-    if (!node) {
-      return nullptr;
-    }
-
-    struct cast_visitor : ast_visitor {
-      void visit(To& obj) override {
-        result = &obj;
-      }
-
-      To* result = nullptr;
-    };
-
-    cast_visitor vis;
-    node->apply_visitor(vis);
-    return vis.result;
-  }
-}
-
 }  // namespace mparse
