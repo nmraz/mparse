7752b4ed5a4a2296c3417e67925263fcbe53e2e9
Noam Raz <noamraz8@gmail.com>
2017-11-17 23:41:48 +0200

    Implement expression pretty printer

---
+++ mparse/src/pretty_print.cpp
@@ -0,0 +1,53 @@
+#include "pretty_print.h"
+
+#include "op_strings.h"
+#include "mparse/ast/ast_visitor.h"
+#include "mparse/ast/literal_node.h"
+#include "mparse/ast/operator_nodes.h"
+#include "mparse/ast/paren_node.h"
+#include <sstream>
+
+using namespace std::literals;
+
+namespace {
+
+struct print_visitor : mparse::ast_visitor {
+  void visit(mparse::unary_node& node) override;
+  void visit(mparse::unary_op_node& node) override;
+  void visit(mparse::binary_op_node& node) override;
+  void visit(mparse::literal_node& node) override;
+
+  std::string result;
+};
+
+void print_visitor::visit(mparse::unary_node& node) {
+  node.child()->apply_visitor(*this);
+}
+
+void print_visitor::visit(mparse::unary_op_node& node) {
+  result = "("s + stringify_unary_op(node.type()) + result + ")";
+}
+
+void print_visitor::visit(mparse::binary_op_node& node) {
+  node.lhs()->apply_visitor(*this);
+  std::string lhs_str = std::move(result);
+
+  node.rhs()->apply_visitor(*this);
+  std::string rhs_str = std::move(result);
+
+  result = "(" + lhs_str + " " + stringify_binary_op(node.type()) + " " + rhs_str + ")";
+}
+
+void print_visitor::visit(mparse::literal_node& node) {
+  std::ostringstream stream;
+  stream << node.val();
+  result = stream.str();
+}
+
+}  // namespace
+
+std::string pretty_print(mparse::abstract_syntax_tree& ast) {
+  print_visitor vis;
+  ast.root()->apply_visitor(vis);
+  return vis.result;
+}
---
+++ mparse/src/pretty_print.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "mparse/ast/abstract_syntax_tree.h"
+#include <string>
+
+std::string pretty_print(mparse::abstract_syntax_tree& ast);
