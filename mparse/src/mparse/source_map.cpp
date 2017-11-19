47cd34daa47c68b20c16ac4e47758a463bb8090c
Noam Raz <noamraz8@gmail.com>
2017-11-19 23:42:54 +0200

    Implement basic source map

---
+++ mparse/src/mparse/source_map.cpp
@@ -0,0 +1,23 @@
+#include "source_map.h"
+
+namespace mparse {
+
+void source_map::set_locs(const ast_node* node, std::vector<source_range> locs) {
+  loc_map_[node] = std::move(locs);
+}
+
+util::span<const source_range> source_map::find_locs(const ast_node* node) const {
+  auto it = loc_map_.find(node);
+
+  if (it == loc_map_.end()) {
+    return {};
+  }
+  return it->second;
+}
+
+
+void source_map::clear() {
+  loc_map_.clear();
+}
+
+}  // namespace mparse
---
+++ mparse/src/mparse/source_map.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "mparse/source_range.h"
+#include "util/span.h"
+#include <unordered_map>
+#include <vector>
+
+namespace mparse {
+
+class ast_node;
+
+class source_map {
+public:
+  void set_locs(const ast_node* node, std::vector<source_range> locs);
+  util::span<const source_range> find_locs(const ast_node* node) const;
+
+  void clear();
+
+private:
+  std::unordered_map<const ast_node*, std::vector<source_range>> loc_map_;
+};
+
+}  // namespace mparse
