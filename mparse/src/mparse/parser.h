7d6b1adbd210b1e45320255f7f3d58e4d8ce72d3
Noam Raz <noamraz8@gmail.com>
2017-11-20 22:40:38 +0200

    Update parser public API to use ast_node_ptr

--- mparse/src/mparse/parser.h
+++ mparse/src/mparse/parser.h
@@ -10,23 +10,21 @@ class parser {
 public:
   explicit parser(source_stream stream);
 
-  void parse_root();
-
   void begin_parse() { get_next_token(); }
 
-  ast_node* parse_add();
-  ast_node* parse_mult();
-  ast_node* parse_unary();
-  ast_node* parse_pow();
+  ast_node_ptr parse_root();
+
+  ast_node_ptr parse_add();
+  ast_node_ptr parse_mult();
+  ast_node_ptr parse_unary();
+  ast_node_ptr parse_pow();
   
-  ast_node* parse_atom();
-  ast_node* consume_literal();
-  ast_node* consume_paren();
+  ast_node_ptr parse_atom();
+  ast_node_ptr consume_literal();
+  ast_node_ptr consume_paren();
 
   const source_stream& stream() const { return stream_; }
   token last_token() const { return last_token_; }
-
-  abstract_syntax_tree& ast() { return ast_; }
 
 private:
   void get_next_token();
@@ -38,10 +36,8 @@ private:
   token last_token_;
 
   const char* expected_type_ = "";  // used for informative error messages
-
-  abstract_syntax_tree ast_;
 };
 
-abstract_syntax_tree parse(std::string_view source);
+ast_node_ptr parse(std::string_view source);
 
 }  // namespace mparse
