f8e78a118c2e45070917cd570b118d9ff9bd607c
Noam Raz <noamraz8@gmail.com>
2017-11-20 23:38:31 +0200

    Add source_map::find_primary_loc

--- mparse/src/mparse/source_map.h
+++ mparse/src/mparse/source_map.h
@@ -12,9 +12,10 @@ class ast_node;
 class source_map {
 public:
   void set_locs(const ast_node* node, std::vector<source_range> locs);
-  util::span<const source_range> find_locs(const ast_node* node) const;
-
   void clear();
+
+  util::span<const source_range> find_locs(const ast_node* node) const;
+  source_range find_primary_loc(const ast_node* node) const { return find_locs(node)[0]; }
 
 private:
   std::unordered_map<const ast_node*, std::vector<source_range>> loc_map_;
