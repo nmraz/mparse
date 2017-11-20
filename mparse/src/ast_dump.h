9e67a6fdeabbddb6a6deb8b8d9aee2915cd19880
Noam Raz <noamraz8@gmail.com>
2017-11-21 00:22:47 +0200

    Update main to take advantage of source maps

--- mparse/src/ast_dump.h
+++ mparse/src/ast_dump.h
@@ -1,5 +1,6 @@
 #pragma once
 
 #include "mparse/ast/ast_node.h"
+#include "mparse/source_map.h"
 
-void dump_ast(const mparse::ast_node* node);
+void dump_ast(const mparse::ast_node* node, const mparse::source_map& smap);
