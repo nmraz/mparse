bf5d9071c50580ce2563b81c86d9f51652058b81
Noam Raz <noamraz8@gmail.com>
2017-11-24 20:38:14 +0200

    Restructure pretty printer

--- mparse/src/ast_ops/pretty_print.cpp
+++ mparse/src/ast_ops/pretty_print.cpp
@@ -5,6 +5,7 @@
 #include "mparse/ast/id_node.h"
 #include "mparse/ast/literal_node.h"
 #include "mparse/ast/operator_nodes.h"
+#include "mparse/ast/paren_node.h"
 #include "util/auto_restore.h"
 #include <sstream>
 
@@ -39,16 +40,33 @@ op_precedence get_precedence(mparse::binary_op_type op) {
   return op_precedence::unknown;  // force parenthesization
 }
 
-std::string parenthesize(std::string expr, op_precedence parent_precedence, op_precedence precedence) {
-  if (static_cast<int>(precedence) < static_cast<int>(parent_precedence)) {
-    return "(" + expr + ")";
+class auto_parenthesizer {
+public:
+  auto_parenthesizer(std::string& expr, op_precedence parent_precedence, op_precedence precedence);
+  ~auto_parenthesizer();
+
+private:
+  std::string& expr_;
+  bool parenthesize_;
+};
+
+auto_parenthesizer::auto_parenthesizer(std::string& expr, op_precedence parent_precedence, op_precedence precedence)
+  : expr_(expr)
+  , parenthesize_(static_cast<int>(precedence) < static_cast<int>(parent_precedence)) {
+  if (parenthesize_) {
+    expr_ += "(";
   }
-  return expr;
+}
+
+auto_parenthesizer::~auto_parenthesizer() {
+  if (parenthesize_) {
+    expr_ += ")";
+  }
 }
 
 
 struct print_visitor : mparse::const_ast_visitor {
-  void visit(const mparse::unary_node& node) override;
+  void visit(const mparse::paren_node& node) override;
   void visit(const mparse::unary_op_node& node) override;
   void visit(const mparse::binary_op_node& node) override;
   void visit(const mparse::literal_node& node) override;
@@ -58,41 +76,41 @@ struct print_visitor : mparse::const_ast_visitor {
   op_precedence parent_precedence = op_precedence::unknown;
 };
 
-void print_visitor::visit(const mparse::unary_node& node) {
+void print_visitor::visit(const mparse::paren_node& node) {
   node.child()->apply_visitor(*this);
 }
 
 void print_visitor::visit(const mparse::unary_op_node& node) {
-  result = parenthesize(stringify_unary_op(node.type()) + result, parent_precedence, op_precedence::unary);
+  print_visitor child_vis;
+  child_vis.parent_precedence = op_precedence::unary;
+  node.child()->apply_visitor(child_vis);
+
+  auto_parenthesizer paren(result, parent_precedence, op_precedence::unary);
+  result += stringify_unary_op(node.type()) + child_vis.result;
 }
 
 void print_visitor::visit(const mparse::binary_op_node& node) {
   mparse::binary_op_type op = node.type();
   op_precedence prec = get_precedence(op);
 
-  std::string lhs_str, rhs_str;
-  {
-    util::auto_restore<op_precedence> save_parent_precedence(parent_precedence);
-    parent_precedence = prec;
+  auto_parenthesizer paren(result, parent_precedence, prec);
 
-    node.lhs()->apply_visitor(*this);
-    lhs_str = std::move(result);
+  util::auto_restore<op_precedence> restore_precedence(parent_precedence);
+  parent_precedence = prec;
 
-    node.rhs()->apply_visitor(*this);
-    rhs_str = std::move(result);
-  }
-
-  result = parenthesize(lhs_str + " " + stringify_binary_op(op) + " " + rhs_str, parent_precedence, prec);
+  node.lhs()->apply_visitor(*this);
+  result += " "s + stringify_binary_op(op) + " ";
+  node.rhs()->apply_visitor(*this);
 }
 
 void print_visitor::visit(const mparse::literal_node& node) {
   std::ostringstream stream;
   stream << node.val();
-  result = stream.str();
+  result += stream.str();
 }
 
 void print_visitor::visit(const mparse::id_node& node) {
-  result = node.name();
+  result += node.name();
 }
 
 }  // namespace
