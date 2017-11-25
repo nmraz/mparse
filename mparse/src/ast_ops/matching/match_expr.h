11e7fe5b07655f2dd033424022f744f10c46a92f
Noam Raz <noamraz8@gmail.com>
2017-11-25 17:52:33 +0200

    Implement basic matcher expression templates

---
+++ mparse/src/ast_ops/matching/match_expr.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "mparse/ast/operator_nodes.h"
+#include <type_traits>
+
+namespace ast_ops::matching {
+
+template<typename T>
+constexpr bool is_match_expr = false;
+
+template<std::size_t I>
+struct unique_symbol_matcher {};
+
+template<std::size_t I>
+constexpr bool is_match_expr<unique_symbol_matcher<I>> = true;
+
+
+struct literal_matcher {
+public:
+  constexpr literal_matcher(double val) : val_(val) {}
+  double val() const { return val_; }
+
+private:
+  double val_;
+};
+
+template<>
+constexpr bool is_match_expr<literal_matcher> = true;
+
+
+template<std::size_t I, typename Node>
+struct node_type_matcher {
+  static_assert(std::is_base_of_v<mparse::ast_node, Node>, "node_type_matcher can only match descendants of ast_node");
+};
+
+template<std::size_t I, typename Node>
+constexpr bool is_match_expr<node_type_matcher<T, Node>> = true;
+
+
+template<mparse::binary_op_type Type, typename Lhs, typename Rhs>
+struct binary_op_matcher {};
+
+template<mparse::binary_op_type Type, typename Lhs, typename Rhs>
+constexpr bool is_match_expr<binary_op_matcher<Type, Lhs, Rhs>> = true;
+
+
+template<mparse::unary_op_type Type, typename Inner>
+struct unary_op_matcher {};
+
+template<mparse::unary_op_type Type, typename Inner>
+constexpr bool is_match_expr<unary_op_matcher<Type, Inner>> = true;
+
+}  // namespace ast_ops::matching
