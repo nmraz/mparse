8d3cfa354d06317e38175be40c905d4487cab613
Noam Raz <noamraz8@gmail.com>
2017-11-30 18:10:52 +0200

    Handle associativity properly in pretty printer

--- mparse/src/ast_ops/pretty_print.cpp
+++ mparse/src/ast_ops/pretty_print.cpp
@@ -44,67 +44,52 @@ op_precedence get_precedence(mparse::binary_op_type op) {
 // whether we are on the right or left-hand side of a subexpression
 enum class branch_side {
   left,
-  right
+  right,
+  none  // unary nodes
 };
 
 // default associativity of unparenthesized operators - left, right, or fully associative
-// (currently only both and left are used)
 enum class associativity {
   left,
   right,
-  both
+  both,
+  none  // unary
 };
 
 associativity get_associativity(mparse::binary_op_type op) {
   switch (op) {
   case mparse::binary_op_type::sub:
   case mparse::binary_op_type::div:
-  case mparse::binary_op_type::pow:
     return associativity::left;
+  case mparse::binary_op_type::pow:
+    return associativity::right;
+  case mparse::binary_op_type::add:
+  case mparse::binary_op_type::mult:
+    return associativity::both;
   }
-  return associativity::both;
+  return associativity::none;
 }
 
-bool should_parenthesize(op_precedence parent_precedence, op_precedence precedence, branch_side side, associativity parent_assoc) {
-  if (precedence == parent_precedence) {  // tie - determine based on associativity
-    if (parent_assoc == associativity::both) {
-      return false;
-    }
-    return (side == branch_side::left && parent_assoc == associativity::right)
-      || (side == branch_side::right && parent_assoc == associativity::left);  // associativity doesn't match side
+bool should_parenthesize_assoc(branch_side side, associativity parent_assoc) {
+  if (parent_assoc == associativity::both) {
+    return false;
   }
+  return parent_assoc == associativity::none
+    || (side == branch_side::left && parent_assoc == associativity::right)
+    || (side == branch_side::right && parent_assoc == associativity::left);  // associativity doesn't match side
+}
 
+bool should_parenthesize(op_precedence parent_precedence, op_precedence precedence, bool assoc_paren) {
+  if (precedence == parent_precedence) {  // tie - determine based on associativity
+    return assoc_paren;
+  }
   return static_cast<int>(precedence) < static_cast<int>(parent_precedence);
 }
 
 
-class auto_parenthesizer {
-public:
-  auto_parenthesizer(std::string& expr, op_precedence parent_precedence, op_precedence precedence);
-  ~auto_parenthesizer();
-
-private:
-  std::string& expr_;
-  bool parenthesize_;
-};
-
-auto_parenthesizer::auto_parenthesizer(std::string& expr, op_precedence parent_precedence, op_precedence precedence)
-  : expr_(expr)
-  , parenthesize_(static_cast<int>(precedence) < static_cast<int>(parent_precedence)) {
-  if (parenthesize_) {
-    expr_ += "(";
-  }
-}
-
-auto_parenthesizer::~auto_parenthesizer() {
-  if (parenthesize_) {
-    expr_ += ")";
-  }
-}
-
-
 struct print_visitor : mparse::const_ast_visitor {
-  print_visitor(mparse::source_map* smap, op_precedence parent_precedence, std::size_t loc_offset = 0);
+  print_visitor(op_precedence parent_precedence, branch_side side, associativity parent_assoc,
+    mparse::source_map* smap, std::size_t loc_offset = 0);
 
   void visit(const mparse::paren_node& node) override;
   void visit(const mparse::unary_op_node& node) override;
@@ -121,16 +106,46 @@ struct print_visitor : mparse::const_ast_visitor {
     return { begin + loc_offset, end + loc_offset };
   }
 
+  op_precedence parent_precedence;
+  bool assoc_paren;
+
   mparse::source_map* smap;
-  op_precedence parent_precedence = op_precedence::unknown;
   std::size_t loc_offset;
 
   std::string result;
 };
 
-print_visitor::print_visitor(mparse::source_map* smap, op_precedence parent_precedence, std::size_t loc_offset)
-  : smap(smap)
-  , parent_precedence(parent_precedence)
+
+class auto_parenthesizer {
+public:
+  auto_parenthesizer(print_visitor& vis, op_precedence precedence);
+  ~auto_parenthesizer();
+
+private:
+  std::string& expr_;
+  bool parenthesize_;
+};
+
+auto_parenthesizer::auto_parenthesizer(print_visitor& vis, op_precedence precedence)
+  : expr_(vis.result)
+  , parenthesize_(should_parenthesize(vis.parent_precedence, precedence, vis.assoc_paren)) {
+  if (parenthesize_) {
+    expr_ += "(";
+  }
+}
+
+auto_parenthesizer::~auto_parenthesizer() {
+  if (parenthesize_) {
+    expr_ += ")";
+  }
+}
+
+
+print_visitor::print_visitor(op_precedence parent_precedence, branch_side side, associativity parent_assoc,
+  mparse::source_map* smap, std::size_t loc_offset)
+  : parent_precedence(parent_precedence)
+  , assoc_paren(should_parenthesize_assoc(side, parent_assoc))
+  , smap(smap)
   , loc_offset(loc_offset) {
 }
 
@@ -143,12 +158,12 @@ void print_visitor::visit(const mparse::paren_node& node) {
 }
 
 void print_visitor::visit(const mparse::unary_op_node& node) {
-  print_visitor child_vis(smap, op_precedence::unary, result.size());
+  print_visitor child_vis(op_precedence::unary, branch_side::none, associativity::none, smap, result.size());
   node.child()->apply_visitor(child_vis);
 
   mparse::source_range op_loc;
   mparse::source_range expr_loc = record_loc([&] {
-    auto_parenthesizer paren(result, parent_precedence, op_precedence::unary);
+    auto_parenthesizer paren(*this, op_precedence::unary);
     
     op_loc = record_loc([&] {
       result += stringify_unary_op(node.type());
@@ -165,19 +180,25 @@ void print_visitor::visit(const mparse::unary_op_node& node) {
 void print_visitor::visit(const mparse::binary_op_node& node) {
   mparse::binary_op_type op = node.type();
   op_precedence prec = get_precedence(op);
+  associativity assoc = get_associativity(op);
 
-  print_visitor child_vis(smap, prec, result.size());
+  std::string lhs_str, rhs_str;
 
-  node.lhs()->apply_visitor(child_vis);
-  std::string lhs_str = std::move(child_vis.result);
+  {
+    print_visitor lhs_vis(prec, branch_side::left, assoc, smap, result.size());
+    node.lhs()->apply_visitor(lhs_vis);
+    lhs_str = std::move(lhs_vis.result);
+  }
 
-  child_vis.result.clear();
-  node.rhs()->apply_visitor(child_vis);
-  std::string rhs_str = std::move(child_vis.result);
+  {
+    print_visitor rhs_vis(prec, branch_side::right, assoc, smap, result.size());
+    node.rhs()->apply_visitor(rhs_vis);
+    rhs_str = std::move(rhs_vis.result);
+  }
 
   mparse::source_range op_loc;
   mparse::source_range expr_loc = record_loc([&] {
-    auto_parenthesizer paren(result, parent_precedence, prec);
+    auto_parenthesizer paren(*this, prec);
 
     result += lhs_str + " ";
 
@@ -220,7 +241,7 @@ void print_visitor::visit(const mparse::id_node& node) {
 
 
 std::string pretty_print(const mparse::ast_node* node, mparse::source_map* smap) {
-  print_visitor vis(smap, op_precedence::unknown);
+  print_visitor vis(op_precedence::unknown, branch_side::none, associativity::none, smap);
   node->apply_visitor(vis);
   return vis.result;
 }
