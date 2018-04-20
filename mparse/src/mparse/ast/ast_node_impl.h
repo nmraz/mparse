f68a9981647b5c5d81389c0e95692570915d5b76
Noam Raz <noamraz8@gmail.com>
2018-04-20 18:18:07 +0300

    Add static assertion to help detect missing overloads of ast_visitor::visit

--- mparse/src/mparse/ast/ast_node_impl.h
+++ mparse/src/mparse/ast/ast_node_impl.h
@@ -4,11 +4,27 @@
 #include <type_traits>
 
 namespace mparse {
+namespace impl {
+
+template<typename V, typename T, typename = void>
+constexpr bool has_visit_overload = false;
+
+template<typename V, typename T>
+constexpr bool has_visit_overload<
+  V,
+  T,
+  std::void_t<
+    decltype(static_cast<void(V::*)(T&)>(&V::visit))
+  >
+> = true;
+
+}  // namespace impl
 
 template<typename Der, typename Base = ast_node>
 class ast_node_impl : public Base {
 public:
   static_assert(std::is_base_of_v<ast_node, Base>, "AST nodes must derive from ast_node");
+  static_assert(impl::has_visit_overload<ast_visitor, Der>, "Missing ast_visitor overload");
 
   void apply_visitor(ast_visitor& vis) override {
     Base::apply_visitor(vis);  // visit bases first
