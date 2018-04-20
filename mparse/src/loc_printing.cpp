ab69a1cef5117d314c694c3f77687d549729b6e8
Noam Raz <noamraz8@gmail.com>
2018-04-20 23:51:52 +0300

    Add sanity check to location printer

--- mparse/src/loc_printing.cpp
+++ mparse/src/loc_printing.cpp
@@ -1,6 +1,7 @@
 #include "loc_printing.h"
 
 #include <algorithm>
+#include <cassert>
 #include <iostream>
 #include <string>
 #include <type_traits>
@@ -19,6 +20,7 @@ void print_locs(std::string_view source, util::span<const mparse::source_range> 
 
   mparse::source_range prev_loc;
   for (const auto& loc : locs) {
+    assert(prev_loc.to() <= loc.from() && "Overlapping or non-sorted source ranges");
     std::cout << std::string(loc.from() - prev_loc.to(), ' ')
       << make_marker(loc.to() - loc.from());
     prev_loc = loc;
