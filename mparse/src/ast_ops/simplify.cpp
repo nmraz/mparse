20da16896852dc57acf6b292a173e179e2b6cc9e
Noam Raz <noamraz8@gmail.com>
2018-07-23 14:24:47 +0300

    Remove matching-related files

--- mparse/src/ast_ops/simplify.cpp
+++
@@ -1,5 +0,0 @@
-#include "simplify.h"
-
-namespace mparse::ast_ops {
-
-}  // namespace mparse::ast_ops
--- mparse/src/ast_ops/simplify.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-namespace mparse::ast_ops {
-
-}  // namespace mparse::ast_ops
