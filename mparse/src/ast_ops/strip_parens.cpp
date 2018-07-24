fbaa06ac17e031091b601afb774e047475a71267
Noam Raz <noamraz8@gmail.com>
2018-07-24 14:00:55 +0300

    Implement parenthesis stripping

--- mparse/src/ast_ops/strip_parens.cpp
+++ mparse/src/ast_ops/strip_parens.cpp
@@ -1,5 +1,47 @@
 #include "strip_parens.h"
 
+#include "mparse/ast/ast_visitor.h"
+#include "mparse/ast/cast.h"
+#include "mparse/ast/operator_nodes.h"
+#include "mparse/ast/paren_node.h"
+#include "mparse/ast/unary_node.h"
 
 namespace ast_ops {
+namespace {
+
+mparse::ast_node_ptr strip_paren(mparse::ast_node_ptr node) {
+  if (auto* paren_node = mparse::ast_node_cast<mparse::paren_node>(node.get())) {
+    return paren_node->take_child();
+  }
+  return node;
+}
+
+
+struct strip_paren_visitor : mparse::ast_visitor {
+  void visit(mparse::unary_node& node) override;
+  void visit(mparse::binary_op_node& node) override;
+};
+
+void strip_paren_visitor::visit(mparse::unary_node& node) {
+  node.child()->apply_visitor(*this);
+  node.set_child(strip_paren(node.take_child()));
+}
+
+void strip_paren_visitor::visit(mparse::binary_op_node& node) {
+  node.lhs()->apply_visitor(*this);
+  node.set_lhs(strip_paren(node.take_lhs()));
+
+  node.rhs()->apply_visitor(*this);
+  node.set_rhs(strip_paren(node.take_rhs()));
+}
+
+}  // namespace
+
+
+void strip_paren(mparse::ast_node_ptr& node) {
+  strip_paren_visitor vis;
+  node->apply_visitor(vis);
+  node = strip_paren(std::move(node));
+}
+
 }  // namespace ast_ops
