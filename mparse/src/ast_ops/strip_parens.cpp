48e7a5addc683dac5a8dbd84db6f4b61ce478c4d
Noam Raz <noamraz8@gmail.com>
2018-07-24 21:35:42 +0300

    Rename parenthesis-stripping helper function

--- mparse/src/ast_ops/strip_parens.cpp
+++ mparse/src/ast_ops/strip_parens.cpp
@@ -10,7 +10,7 @@
 namespace ast_ops {
 namespace {
 
-mparse::ast_node_ptr strip_paren(mparse::ast_node_ptr node) {
+mparse::ast_node_ptr strip_paren_step(mparse::ast_node_ptr node) {
   if (auto* paren_node = mparse::ast_node_cast<mparse::paren_node>(node.get())) {
     return paren_node->take_child();
   }
@@ -26,21 +26,21 @@ struct strip_paren_visitor : mparse::ast_visitor {
 
 void strip_paren_visitor::visit(mparse::unary_node& node) {
   node.child()->apply_visitor(*this);
-  node.set_child(strip_paren(node.take_child()));
+  node.set_child(strip_paren_step(node.take_child()));
 }
 
 void strip_paren_visitor::visit(mparse::binary_op_node& node) {
   node.lhs()->apply_visitor(*this);
-  node.set_lhs(strip_paren(node.take_lhs()));
+  node.set_lhs(strip_paren_step(node.take_lhs()));
 
   node.rhs()->apply_visitor(*this);
-  node.set_rhs(strip_paren(node.take_rhs()));
+  node.set_rhs(strip_paren_step(node.take_rhs()));
 }
 
 void strip_paren_visitor::visit(mparse::func_node& node) {
   for (auto& arg : node.args()) {
     arg->apply_visitor(*this);
-    arg = strip_paren(arg);
+    arg = strip_paren_step(arg);
   }
 }
 
@@ -50,7 +50,7 @@ void strip_paren_visitor::visit(mparse::func_node& node) {
 void strip_parens(mparse::ast_node_ptr& node) {
   strip_paren_visitor vis;
   node->apply_visitor(vis);
-  node = strip_paren(std::move(node));
+  node = strip_paren_step(std::move(node));
 }
 
 }  // namespace ast_ops
