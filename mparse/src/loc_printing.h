69f422176b7cf67e9e0eb384737ef4e52f110def
Noam Raz <noamraz8@gmail.com>
2018-07-24 14:19:29 +0300

    Add deduction guides for util::span

--- mparse/src/loc_printing.h
+++ mparse/src/loc_printing.h
@@ -8,7 +8,7 @@
 void print_locs(std::string_view source, util::span<const mparse::source_range> locs);
 
 inline void print_locs(std::string_view source, std::initializer_list<mparse::source_range> locs) {
-  print_locs(source, util::make_span(&*locs.begin(), locs.size()));
+  print_locs(source, util::span(&*locs.begin(), locs.size()));
 }
 
 inline void print_loc(std::string_view source, mparse::source_range loc) {
