1715f7cdbe3ac931afa83a96e5d06bc06e8f9841
Noam Raz <noamraz8@gmail.com>
2018-07-25 23:03:24 +0300

    Add support for fixit hints

--- mparse/src/mparse/parse_error.cpp
+++ mparse/src/mparse/parse_error.cpp
@@ -2,9 +2,12 @@
 
 namespace mparse {
 
-syntax_error::syntax_error(std::string_view what, std::vector<source_range> where)
+syntax_error::syntax_error(std::string_view what, std::vector<source_range> where,
+  std::string fixit_hint, int fixit_col)
   : std::runtime_error(what.data())
-  , where_(std::move(where)) {
+  , where_(std::move(where)) 
+  , fixit_hint_(fixit_hint) 
+  , fixit_col_(fixit_col) {
 }
 
 }  // namespace mparse
--- mparse/src/mparse/parse_error.h
+++ mparse/src/mparse/parse_error.h
@@ -3,18 +3,25 @@
 #include "mparse/source_range.h"
 #include "util/span.h"
 #include <stdexcept>
+#include <string>
+#include <string_view>
 #include <vector>
 
 namespace mparse {
 
 class syntax_error : public std::runtime_error {
 public:
-  syntax_error(std::string_view what, std::vector<source_range> where);
+  syntax_error(std::string_view what, std::vector<source_range> where, 
+    std::string fixit_hint = "", int fixit_col = 0);
 
   util::span<const source_range> where() const { return where_; }
+  std::string_view fixit_hint() const { return fixit_hint_; }
+  int fixit_col() const { return fixit_col_; }
 
 private:
   std::vector<source_range> where_;
+  std::string fixit_hint_;
+  int fixit_col_;
 };
 
 }  // namespace mparse
