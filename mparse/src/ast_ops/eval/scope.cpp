ab20bcfb0b9a3ee003ddeb9dff859a2d6f123314
Noam Raz <noamraz8@gmail.com>
2018-07-25 12:36:35 +0300

    Add func_scope

--- mparse/src/ast_ops/eval/scope.cpp
+++ mparse/src/ast_ops/eval/scope.cpp
@@ -25,4 +25,28 @@ std::optional<double> var_scope::lookup(std::string_view name) const {
   return std::nullopt;
 }
 
+
+func_scope::func_scope(std::initializer_list<impl_type::value_type> ilist) 
+  : map_(ilist) {
+}
+
+void func_scope::set_binding(std::string name, func_type func) {
+  map_[std::move(name)] = std::move(func);
+}
+
+void func_scope::remove_binding(std::string_view name) {
+  auto it = map_.find(name);
+  if (it != map_.end()) {
+    map_.erase(it);
+  }
+}
+
+const func_scope::func_type* func_scope::lookup(std::string_view name) const {
+  auto it = map_.find(name);
+  if (it != map_.end()) {
+    return &it->second;
+  }
+  return nullptr;
+}
+
 }  // namespace ast_ops
--- mparse/src/ast_ops/eval/scope.h
+++ mparse/src/ast_ops/eval/scope.h
@@ -1,9 +1,12 @@
 #pragma once
 
+#include <functional>
+#include <initializer_list>
 #include <map>
 #include <optional>
 #include <string>
 #include <string_view>
+#include <vector>
 
 namespace ast_ops {
 
@@ -20,6 +23,29 @@ public:
   void clear() { map_.clear(); }
 
   std::optional<double> lookup(std::string_view name) const;
+
+private:
+  impl_type map_;
+};
+
+
+class func_scope {
+public:
+  using func_type = std::function<double(std::vector<double>)>;
+
+private:
+  using impl_type = std::map<std::string, func_type, std::less<>>;
+
+public:
+  func_scope() = default;
+  func_scope(std::initializer_list<impl_type::value_type> ilist);
+
+  void set_binding(std::string name, func_type func);
+  void remove_binding(std::string_view name);
+
+  void clear() { map_.clear(); }
+
+  const func_type* lookup(std::string_view name) const;
 
 private:
   impl_type map_;
