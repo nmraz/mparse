1c3e186041465b6790d1e21b4e83ce967d662054
Noam Raz <noamraz8@gmail.com>
2018-07-26 18:36:52 +0300

    Add real_function typedef

--- mparse/src/ast_ops/eval/types.h
+++ mparse/src/ast_ops/eval/types.h
@@ -8,5 +8,6 @@ namespace ast_ops {
 
 using number = std::complex<double>;
 using function = std::function<number(std::vector<number>)>;
+using real_function = std::function<number(std::vector<double>)>;
 
 }  // namespace ast_ops
