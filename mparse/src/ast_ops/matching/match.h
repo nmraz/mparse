166b2497f248742d5bd40148d46c43e36b1254f7
Noam Raz <noamraz8@gmail.com>
2018-07-28 16:36:51 +0300

    Begin reimplementing matching

--- mparse/src/ast_ops/matching/match.h
+++ mparse/src/ast_ops/matching/match.h
@@ -3,9 +3,129 @@
 #include "ast_ops/matching/match_expr.h"
 #include "ast_ops/matching/match_results.h"
 #include "mparse/ast/ast_node.h"
+#include "mparse/ast/cast.h"
+#include "mparse/ast/literal_node.h"
+#include "mparse/ast/operator_nodes.h"
+#include "util/meta.h"
 #include <optional>
 
 namespace ast_ops::matching {
+
+template<typename E>
+struct matcher_traits {
+  static_assert(util::always_false<E>, "Unknown expression type");
+};
+
+template<>
+struct matcher_traits<literal_expr> {
+  using match_type = mparse::literal_node;
+  
+  template<typename Ctx>
+  static bool match(const literal_expr& expr, const mparse::ast_node_ptr& node, Ctx&) {
+    if (auto* lit_node = mparse::ast_node_cast<match_type>(node.get())) {
+      return lit_node->val() == expr.val;
+    }
+    return false;
+  }
+};
+
+template<typename Node>
+struct matcher_traits<node_type_expr<Node>> {
+  using match_type = Node;
+
+  template<typename Ctx>
+  static bool match(const node_type_expr<Node>&, const mparse::ast_node_ptr& node, Ctx&) {
+    return mparse::ast_node_cast<match_type>(node.get()) != nullptr;
+  }
+};
+
+template<typename Pred, typename Lhs, typename Rhs, bool Commute>
+struct matcher_traits<binary_op_pred_expr<Pred, Lhs, Rhs, Commute>> {
+  using match_type = mparse::binary_op_node;
+
+  template<typename Ctx>
+  static bool match(const binary_op_expr<Pred, Lhs, Rhs, Commute>& expr, const mparse::ast_node_ptr& node, Ctx& ctx) {
+    if (auto* bin_node = mparse::ast_node_cast<match_type>(node.get())) {
+
+      if (!Pred{}(bin_node->type())) {
+        return false;
+      }
+
+      auto node_lhs = bin_node->ref_lhs();
+      auto node_rhs = bin_node->ref_rhs();
+
+      if (matcher_traits<Lhs>::match(expr.lhs, node_lhs, ctx)
+        && matcher_traits<Rhs>::match(expr.rhs, node_rhs, ctx)) {
+        return true;
+      }
+
+      if (Commute && matcher_traits<Lhs>::match(expr.lhs, node_rhs, ctx) 
+        && matcher_traits<Rhs>::match(expr.rhs, node_lhs, ctx)) {
+        return true;
+      }
+
+      return false;
+    }
+    return false;
+  }
+};
+
+template<typename Node, typename Inner>
+struct matcher_traits<unary_expr<Node, Inner>> {
+  using match_type = Node;
+
+  template<typename Ctx>
+  static bool match(const unary_expr<Node, Inner>& expr, const mparse::ast_node_ptr& node, Ctx& ctx) {
+    if (auto* un_node = mparse::ast_node_cast<match_type>(node.get())) {
+      return matcher_traits<Inner>::match(expr.inner, un_node->ref_child(), ctx)
+    }
+    return false;
+  }
+};
+
+template<typename Pred, typename Inner>
+struct matcher_traits<unary_op_pred_expr<Pred, Inner>> {
+  using match_type = mparse::unary_op_node;
+
+  template<typename Ctx>
+  static bool match(const unary_op_pred_expr<Pred, Inner>& expr, const mparse::ast_node_ptr& node, Ctx& ctx) {
+    if (auto* un_node = mparse::ast_node_cast<match_type>(node.get())) {
+      if (!Pred{}(un_node->type())) {
+        return false;
+      }
+
+      return matcher_traits<Inner>::match(expr.inner, un_node->ref_child(), ctx);
+    }
+    return false;
+  }
+};
+
+template<typename Expr>
+struct matcher_traits<negation_expr<Expr>> {
+  template<typename Ctx>
+  static bool match(const negation_expr<Expr>& expr, const mparse::ast_node_ptr& node, Ctx& ctx) {
+    return !matcher_traits<Expr>::match(expr.expr, node, ctx);
+  }
+};
+
+template<typename First, typename Second>
+struct matcher_traits<conjunction_expr<First, Second>> {
+   template<typename Ctx>
+   static bool match(const conjunction_expr<First, Second>& expr, const mparse::ast_node_ptr& node, Ctx& ctx) {
+     return matcher_traits<First>::match(expr.first, node, ctx)
+       && matcher_traits<Second>::match(expr.second, node, ctx);
+   }
+};
+
+template<typename First, typename Second>
+struct matcher_traits<disjunction_expr<First, Second>> {
+  template<typename Ctx>
+  static bool match(const disjunction_expr<First, Second>& expr, const mparse::ast_node_ptr& node, Ctx& ctx) {
+    return matcher_traits<First>::match(expr.first, node, ctx)
+      || matcher_traits<Second>::match(expr.second, node, ctx);
+  }
+};
+
 
 
 template<typename Matcher, typename = std::enable_if_t<is_match_expr<Matcher>>>
