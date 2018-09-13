4c2936a89aa8c690ebf39d046015079e04f93385
Noam Raz <noamraz8@gmail.com>
2018-09-13 22:44:08 +0300

    Add basic expression rewriter

--- mparse/src/ast_ops/matching/rewrite.h
+++ mparse/src/ast_ops/matching/rewrite.h
@@ -1,5 +1,17 @@
 #pragma once
 
+#include "ast_ops/matching/build.h"
+#include "ast_ops/matching/match.h"
+#include "ast_ops/matching/match_results.h"
+#include "mparse/ast/ast_node.h"
+
 namespace ast_ops::matching {
+
+template<typename M, typename B>
+void rewrite(mparse::ast_node_ptr& node, const M& matcher, const B& builder) {
+  if (auto res = exec_match(matcher, node)) {
+    node = build_expr(builder, res);
+  }
+}
 
 }  // namespace ast_ops::matching
