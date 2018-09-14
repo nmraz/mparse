87df0d262908000e36e867a0679e9bca6932f28c
Noam Raz <noamraz8@gmail.com>
2018-09-15 00:04:51 +0300

    Add rewriter class

--- mparse/src/ast_ops/matching/rewrite.cpp
+++ mparse/src/ast_ops/matching/rewrite.cpp
@@ -1,5 +1,15 @@
 #include "rewrite.h"
 
+#include <utility>
+
 namespace ast_ops::matching {
+
+rewriter::rewriter(rewrite_func func)
+  : func_(std::move(func)) {
+}
+
+void rewriter::apply(mparse::ast_node_ptr& node) const {
+  func_(node);
+}
 
 }  // namespace ast_ops::matching
--- mparse/src/ast_ops/matching/rewrite.h
+++ mparse/src/ast_ops/matching/rewrite.h
@@ -4,6 +4,7 @@
 #include "ast_ops/matching/match.h"
 #include "ast_ops/matching/match_results.h"
 #include "mparse/ast/ast_node.h"
+#include <functional>
 
 namespace ast_ops::matching {
 
@@ -13,5 +14,26 @@ void rewrite(mparse::ast_node_ptr& node, const M& matcher, const B& builder) {
     node = build_expr(builder, res);
   }
 }
+
+
+using rewrite_func = std::function<void(mparse::ast_node_ptr&)>;
+
+class rewriter {
+public:
+  template<typename M, typename B>
+  rewriter(const M& matcher, const B& builder);
+  rewriter(rewrite_func func);
+
+  void apply(mparse::ast_node_ptr& node) const;
+
+private:
+  rewrite_func func_;
+};
+
+template<typename M, typename B>
+rewriter::rewriter(const M& matcher, const B& builder)
+  : rewriter([matcher, builder](mparse::ast_node_ptr& node) {
+    rewrite(node, matcher, builder);
+  }) {}
 
 }  // namespace ast_ops::matching
