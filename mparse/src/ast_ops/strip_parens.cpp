6163a65e9141f077deff4c347ce62c905674f22e
Noam Raz <noamraz8@gmail.com>
2018-09-17 19:07:35 +0300

    Shorten strip_parens

--- mparse/src/ast_ops/strip_parens.cpp
+++ mparse/src/ast_ops/strip_parens.cpp
@@ -3,18 +3,14 @@
 #include "ast_ops/matching/rewrite.h"
 
 namespace ast_ops {
-namespace {
-
-using namespace ast_ops::matching::literals;
-
-constexpr ast_ops::matching::rewriter_list rewriters = {
-  paren(x), x
-};
-
-}  // namespace
-
 
 void strip_parens(mparse::ast_node_ptr& node) {
+  using namespace ast_ops::matching::literals;
+
+  static constexpr ast_ops::matching::rewriter_list rewriters = {
+    paren(x), x
+  };
+
   ast_ops::matching::apply_rewriters_recursively(node, rewriters);
 }
 
