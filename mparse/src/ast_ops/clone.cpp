15d496babc91a2f417e588749e150df3775a7b37
Noam Raz <noamraz8@gmail.com>
2018-09-18 15:52:39 +0300

    Add declarations for AST clone

--- mparse/src/ast_ops/clone.cpp
+++ mparse/src/ast_ops/clone.cpp
@@ -1,5 +1,31 @@
 #include "clone.h"
 
+#include "mparse/ast/ast_visitor.h"
+#include "mparse/ast/abs_node.h"
+#include "mparse/ast/func_node.h"
+#include "mparse/ast/id_node.h"
+#include "mparse/ast/literal_node.h"
+#include "mparse/ast/operator_nodes.h"
+#include "mparse/ast/paren_node.h"
+
 namespace ast_ops {
+namespace {
+
+struct clone_visitor : mparse::const_ast_visitor {
+  void visit(const mparse::paren_node& node) override;
+  void visit(const mparse::abs_node& node) override;
+  void visit(const mparse::unary_op_node& node) override;
+  void visit(const mparse::binary_op_node& node) override;
+  void visit(const mparse::func_node& node) override;
+  void visit(const mparse::literal_node& node) override;
+  void visit(const mparse::id_node& node) override;
+};
+
+}  // namespace
+
+
+mparse::ast_node_ptr clone(const mparse::ast_node* node) {
+
+}
 
 }  // namespace ast_ops
--- mparse/src/ast_ops/clone.h
+++ mparse/src/ast_ops/clone.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "mparse/ast/ast_node.h"
+
 namespace ast_ops {
+
+mparse::ast_node_ptr clone(const mparse::ast_node* node);
 
 }  // namespace ast_ops
