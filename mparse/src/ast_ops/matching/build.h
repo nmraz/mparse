e7f3bcda5b975d99840eff80c595da03e2968e24
Noam Raz <noamraz8@gmail.com>
2018-09-19 22:40:06 +0300

    Add support for tag extraction to builders

--- mparse/src/ast_ops/matching/build.h
+++ mparse/src/ast_ops/matching/build.h
@@ -18,7 +18,7 @@ struct builder_traits {
 
 template<typename F>
 struct builder_traits<custom_builder_expr<F>> {
-  template<typename Ctx>
+  template<typename BuildTags, typename Ctx>
   static auto build(const custom_builder_expr<F>& expr, const Ctx& ctx) {
     return expr.func(ctx);
   }
@@ -26,7 +26,7 @@ struct builder_traits<custom_builder_expr<F>> {
 
 template<>
 struct builder_traits<literal_expr> {
-  template<typename Ctx>
+  template<typename BuildTags, typename Ctx>
   static auto build(const literal_expr& expr, const Ctx&) {
     return mparse::make_ast_node<mparse::literal_node>(expr.val);
   }
@@ -34,7 +34,7 @@ struct builder_traits<literal_expr> {
 
 template<>
 struct builder_traits<id_expr> {
-  template<typename Ctx>
+  template<typename BuildTags, typename Ctx>
   static auto build(const id_expr& expr, const Ctx&) {
     return mparse::make_ast_node<mparse::id_node>(std::string(expr.name));
   }
@@ -43,48 +43,48 @@ struct builder_traits<id_expr> {
 template<typename... Args>
 struct builder_traits<func_expr<Args...>> {
 private:
-  template<typename... Args, size_t... I, typename Ctx>
+  template<typename BuildTags, typename... Args, size_t... I, typename Ctx>
   static mparse::func_node::arg_list build_args(const std::tuple<Args...>& args,
     std::index_sequence<I...>, Ctx& ctx) {
     mparse::func_node::arg_list arg_nodes;
-    (..., arg_nodes.push_back(builder_traits<Args>::build(std::get<I>(args), ctx)));
+    (..., arg_nodes.push_back(builder_traits<Args>::template build<BuildTags>(std::get<I>(args), ctx)));
     return arg_nodes;
   }
 
 public:
-  template<typename Ctx>
+  template<typename BuildTags, typename Ctx>
   static auto build(const func_expr<Args...>& expr, const Ctx& ctx) {
     return mparse::make_ast_node<mparse::func_node>(
       std::string(expr.name),
-      build_args(expr.args, std::index_sequence_for<Args...>{}, ctx)
+      build_args<BuildTags>(expr.args, std::index_sequence_for<Args...>{}, ctx)
     );
   }
 };
 
 template<typename Node, typename Inner>
 struct builder_traits<unary_expr<Node, Inner>> {
-  template<typename Ctx>
+  template<typename BuildTags, typename Ctx>
   static auto build(const unary_expr<Node, Inner>& expr, const Ctx& ctx) {
-    auto inner_node = builder_traits<Inner>::build(expr.inner, ctx);
+    auto inner_node = builder_traits<Inner>::template build<BuildTags>(expr.inner, ctx);
     return mparse::make_ast_node<Node>(std::move(inner_node));
   }
 };
 
 template<mparse::unary_op_type Type, typename Inner>
 struct builder_traits<unary_op_expr<Type, Inner>> {
-  template<typename Ctx>
+  template<typename BuildTags, typename Ctx>
   static auto build(const unary_op_expr<Type, Inner>& expr, const Ctx& ctx) {
-    auto inner_node = builder_traits<Inner>::build(expr.inner, ctx);
+    auto inner_node = builder_traits<Inner>::template build<BuildTags>(expr.inner, ctx);
     return mparse::make_ast_node<mparse::unary_op_node>(Type, std::move(inner_node));
   }
 };
 
 template<mparse::binary_op_type Type, typename Lhs, typename Rhs, bool Commute>
 struct builder_traits<binary_op_expr<Type, Lhs, Rhs, Commute>> {
-  template<typename Ctx>
+  template<typename BuildTags, typename Ctx>
   static auto build(const binary_op_expr<Type, Lhs, Rhs, Commute>& expr, const Ctx& ctx) {
-    auto lhs_node = builder_traits<Lhs>::build(expr.lhs, ctx);
-    auto rhs_node = builder_traits<Rhs>::build(expr.rhs, ctx);
+    auto lhs_node = builder_traits<Lhs>::template build<BuildTags>(expr.lhs, ctx);
+    auto rhs_node = builder_traits<Rhs>::template build<BuildTags>(expr.rhs, ctx);
 
     return mparse::make_ast_node<mparse::binary_op_node>(Type, std::move(lhs_node),
       std::move(rhs_node));
@@ -93,7 +93,7 @@ struct builder_traits<binary_op_expr<Type, Lhs, Rhs, Commute>> {
 
 template<typename Tag, typename Expr>
 struct builder_traits<capture_expr_impl<Tag, Expr>> {
-  template<typename Ctx>
+  template<typename BuildTags, typename Ctx>
   static auto build(const capture_expr_impl<Tag, Expr>& expr, const Ctx& ctx) {
     return get_result<Tag>(ctx);
   }
@@ -101,16 +101,35 @@ struct builder_traits<capture_expr_impl<Tag, Expr>> {
 
 template<char C, typename Comp>
 struct builder_traits<subexpr_expr<C, Comp>> {
-  template<typename Ctx>
+  template<typename BuildTags, typename Ctx>
   static auto build(const subexpr_expr<C, Comp>&, const Ctx& ctx) {
     return get_subexpr<C>(ctx);
   }
 };
 
 
+namespace impl {
+
+template<typename E, typename = void>
+struct get_build_tags {
+  using type = caplist<>;
+};
+
+template<typename E>
+struct get_build_tags<E, std::void_t<typename builder_traits<E>::tags>> {
+  using type = typename builder_traits<E>::tags;
+};
+
+template<typename E>
+using get_build_tags_t = typename get_build_tags<E>::type;
+
+}  // namespace impl
+
+
 template<typename Expr, typename Ctx>
 mparse::ast_node_ptr build_expr(Expr expr, const Ctx& ctx) {
-  return builder_traits<Expr>::build(expr, ctx);
+  using build_tags = impl::get_build_tags_t<Expr>;
+  return builder_traits<Expr>::template build<build_tags>(expr, ctx);
 }
 
 }  // namespace ast_ops::matching
