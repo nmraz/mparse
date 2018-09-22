6dc2df9d45f58fd4c4b931dbc2b1dd8223110ab0
Noam Raz <noamraz8@gmail.com>
2018-09-22 22:55:58 +0300

    Implement basic canonicalization

--- mparse/src/ast_ops/simplify.cpp
+++ mparse/src/ast_ops/simplify.cpp
@@ -2,26 +2,53 @@
 
 #include "ast_ops/matching/rewrite.h"
 
+using namespace ast_ops::matching::literals;
+
 namespace ast_ops {
+namespace {
+
+// clang-format off
+
+constexpr ast_ops::matching::rewriter_list strip_paren_rewriters = {
+    paren(x), x
+};
+
+
+constexpr ast_ops::matching::rewriter_list canon_op_rewriters = {
+    +x, x,
+
+    x - y, x + -y,
+
+    -capture_as<1>(match_not(matching::node_type_expr<mparse::literal_node>{})),
+    -1_lit * cap<1>, // -x -> -1 * x
+
+    x / y,
+    x * pow(y, -1_lit) // x / y -> x * y^-1
+};
+
+constexpr ast_ops::matching::rewriter_list canon_rewriters = {
+    capture_as<1>(match_not(match_or(pow(x, y), x * y))),
+    pow(cap<1>, 1_lit), // x -> x ^ 1
+
+    capture_as<1>(match_not(x * y)),
+    cap<1> * 1_lit // x -> 1 * x
+};
+
+// clang-format on
+
+} // namespace
+
 
 void strip_parens(mparse::ast_node_ptr& node) {
-  using namespace ast_ops::matching::literals;
-
-  static constexpr ast_ops::matching::rewriter_list rewriters = {
-      paren(x),
-      x,
-  };
-
-  ast_ops::matching::apply_rewriters_recursively(node, rewriters);
+  matching::apply_rewriters_recursively(node, strip_paren_rewriters);
 }
 
 
 void canonicalize(mparse::ast_node_ptr& node) {
-
+  matching::apply_rewriters_recursively(node, canon_op_rewriters);
+  matching::apply_rewriters_recursively(node, canon_rewriters);
 }
 
-void decanonicalize(mparse::ast_node_ptr& node) {
-
-}
+void decanonicalize(mparse::ast_node_ptr& node) {}
 
 } // namespace ast_ops
