49cffd77021cfc9dd84869e6de5acd33fda257d6
Noam Raz <noamraz8@gmail.com>
2018-10-06 12:37:52 +0300

    Remove old code from main

--- mparse/src/main.cpp
+++ mparse/src/main.cpp
@@ -31,17 +31,7 @@ struct subcommand {
 using command_map = std::map<std::string, subcommand, std::less<>>;
 
 void print_help(std::string_view prog_name) {
-  prog_name.remove_prefix(
-      std::min(prog_name.rfind('\\') + 1, prog_name.size()));
-  std::cout << "Usage: " << prog_name
-            << " dump|pretty|strip|eval <expr> [var1=val1 var2=val2 ...]\n\n";
-  std::cout << "dump - Print a visualization of the AST.\n";
-  std::cout << "pretty - Pretty print the expression.\n";
-  std::cout << "strip - Pretty print the expression, with superfluous "
-               "parentheses removed.\n";
-  std::cout << "eval - Evaluate the expression, using the passed variable "
-               "definitions.\n";
-  std::exit(1);
+
 }
 
 auto parse_diag(std::string_view input) {
@@ -60,40 +50,4 @@ auto parse_diag(std::string_view input) {
 
 int main(int argc, const char* const* argv) {
   std::cout << std::setprecision(std::numeric_limits<double>::digits10 + 1);
-
-  if (argc < 3) {
-    print_help(argv[0]);
-  }
-
-  std::string_view cmd = argv[1];
-  if (cmd != "dump" && cmd != "eval" && cmd != "pretty" && cmd != "strip") {
-    print_help(argv[0]);
-  }
-
-  std::string_view input = argv[2];
-  auto [ast, smap] = parse_diag(input);
-
-  if (cmd == "dump") {
-    ast_ops::dump_ast(ast.get(), &smap);
-  } else if (cmd == "pretty") {
-    std::cout << ast_ops::pretty_print(ast.get()) << "\n";
-  } else if (cmd == "strip") {
-    ast_ops::strip_parens(ast);
-    std::cout << ast_ops::pretty_print(ast.get()) << "\n";
-  } else if (cmd == "eval") {
-    ast_ops::var_scope vscope = ast_ops::builtin_var_scope();
-    parse_vardefs(vscope, argc - 3, argv + 3);
-
-    try {
-      auto result =
-          ast_ops::eval(ast.get(), vscope, ast_ops::builtin_func_scope());
-      result.real(to_precision(result.real()));
-      result.imag(to_precision(result.imag()));
-
-      print_number(std::cout, result) << '\n';
-    } catch (const ast_ops::eval_error& err) {
-      handle_math_error(err, smap, input);
-      return 1;
-    }
-  }
 }
