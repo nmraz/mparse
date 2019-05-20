814167b896e0a70ae426e40e3f56ca74b15301f6
Noam Raz <noamraz8@gmail.com>
2019-05-20 21:57:52 +0300

    Remove identity canonicalization

--- mparse/src/ast_ops/simplify.cpp
+++ mparse/src/ast_ops/simplify.cpp
@@ -50,57 +50,14 @@ constexpr auto non_mul = !(any * any || lit_or_neg);
 
 // clang-format off
 
-constexpr matching::rewriter_list canon_op_rewriters = {
+constexpr matching::rewriter_list canon_rewriters = {
     +x, x,
     x - y, x + -1_lit * y,
     -capture_as<1>(!lit), -1_lit * cap<1>,
     x / y, x * pow(y, -1_lit)
 };
 
-constexpr matching::rewriter_list canon_mul_ident_rewriter = {
-    capture_as<1>(non_mul), 1_lit * cap<1>
-};
-
-constexpr matching::rewriter_list canon_pow_ident_rewriter = {
-    capture_as<1>(!pow(any, any)), pow(cap<1>, 1_lit)
-};
-
-// clang-format on
-
-
-void canonicalize_mul_ident(mparse::ast_node_ptr& node);
-
-void canonicalize_pow_ident(mparse::ast_node_ptr& node) {
-  if (matching::exec_match(non_mul, node)) {
-    // insert pow or ignore existing one
-    matching::apply_rewriters(node, canon_pow_ident_rewriter);
-
-    matching::apply_to_children(*node, [](mparse::ast_node_ptr& pow_child) {
-      matching::apply_to_children(*pow_child, [](mparse::ast_node_ptr& child) {
-        canonicalize_mul_ident(child);
-      });
-    });
-  } else {
-    // couldn't add a pow here - try children
-    matching::apply_to_children(*node, [](mparse::ast_node_ptr& child) {
-      canonicalize_pow_ident(child);
-    });
-  }
-}
-
-void canonicalize_mul_ident(mparse::ast_node_ptr& node) {
-  matching::apply_rewriters(node, canon_mul_ident_rewriter);
-
-  // expression is of the form `x * ___`
-  matching::apply_to_children(*node, [](mparse::ast_node_ptr& mul_child) {
-    canonicalize_pow_ident(mul_child);
-  });
-}
-
-
-// clang-format off
-
-constexpr matching::rewriter_list uncanon_op_rewriters = {
+constexpr matching::rewriter_list uncanon_rewriters = {
     -1_lit * x, -x,
     x + -y, x - y,
     
@@ -108,45 +65,18 @@ constexpr matching::rewriter_list uncanon_op_rewriters = {
     x * (1_lit / y), x / y
 };
 
-constexpr matching::rewriter_list uncanon_ident_rewriters = {
-    0_lit * any, 0_lit,
-    x + 0_lit, x,
-
-    pow(any, 0_lit), 1_lit,
-
-    1_lit * x, x,
-    pow(x, 1_lit), x
-};
-
 // clang-format on
-
-
-void uncanonicalize_ident(mparse::ast_node_ptr& node) {
-  matching::apply_rewriters_recursively(node, uncanon_ident_rewriters);
-}
 
 } // namespace
 
 
-void canonicalize_ops(mparse::ast_node_ptr& node) {
-  strip_parens(node);
-  matching::apply_rewriters_recursively(node, canon_op_rewriters);
-}
-
 void canonicalize(mparse::ast_node_ptr& node) {
-  canonicalize_ops(node);
-  canonicalize_mul_ident(node);
-}
-
-
-void uncanonicalize_ops(mparse::ast_node_ptr& node) {
-  matching::apply_rewriters_recursively(node, uncanon_op_rewriters);
+  strip_parens(node);
+  matching::apply_rewriters_recursively(node, canon_rewriters);
 }
 
 void uncanonicalize(mparse::ast_node_ptr& node) {
-  uncanonicalize_ident(node); // initial pass
-  uncanonicalize_ops(node);
-  uncanonicalize_ident(node); // clean up after operator uncanonicalization
+  matching::apply_rewriters_recursively(node, uncanon_rewriters);
 }
 
 
--- mparse/src/ast_ops/simplify.h
+++ mparse/src/ast_ops/simplify.h
@@ -16,10 +16,7 @@ void strip_parens(mparse::ast_node_ptr& node);
 void insert_parens(mparse::ast_node_ptr& node);
 
 
-void canonicalize_ops(mparse::ast_node_ptr& node);
 void canonicalize(mparse::ast_node_ptr& node);
-
-void uncanonicalize_ops(mparse::ast_node_ptr& node);
 void uncanonicalize(mparse::ast_node_ptr& node);
 
 
