a83e8383f139daf2f40b30534de138ee7d03be0e
Noam Raz <noamraz8@gmail.com>
2019-05-22 00:57:20 +0300

    Migrate recursive application to new visitor API

--- mparse/src/ast_ops/matching/rewrite.cpp
+++ mparse/src/ast_ops/matching/rewrite.cpp
@@ -6,23 +6,23 @@
 namespace ast_ops::matching {
 namespace {
 
-struct child_apply_visitor : mparse::ast_visitor {
+struct child_apply_visitor : mparse::ast_visitor1<child_apply_visitor> {
   explicit child_apply_visitor(const basic_rewriter_func& func) : func(func) {}
 
-  void visit(mparse::unary_node& node) override;
-  void visit(mparse::binary_op_node& node) override;
-  void visit(mparse::func_node& node) override;
+  void operator()(mparse::unary_node& node);
+  void operator()(mparse::binary_op_node& node);
+  void operator()(mparse::func_node& node);
 
   const basic_rewriter_func& func;
 };
 
-void child_apply_visitor::visit(mparse::unary_node& node) {
+void child_apply_visitor::operator()(mparse::unary_node& node) {
   auto child = node.ref_child(); // ref here for strong exception guarantee
   func(child);
   node.set_child(std::move(child));
 }
 
-void child_apply_visitor::visit(mparse::binary_op_node& node) {
+void child_apply_visitor::operator()(mparse::binary_op_node& node) {
   {
     auto lhs = node.ref_lhs(); // ref here for strong exception guarantee
     func(lhs);
@@ -36,7 +36,7 @@ void child_apply_visitor::visit(mparse::binary_op_node& node) {
   }
 }
 
-void child_apply_visitor::visit(mparse::func_node& node) {
+void child_apply_visitor::operator()(mparse::func_node& node) {
   for (auto& arg : node.args()) {
     func(arg);
   }
@@ -48,7 +48,7 @@ void child_apply_visitor::visit(mparse::func_node& node) {
 void apply_to_children(mparse::ast_node& node,
                        const basic_rewriter_func& func) {
   child_apply_visitor vis(func);
-  node.apply_visitor(vis);
+  mparse::apply_visitor(vis, node);
 }
 
 } // namespace ast_ops::matching
